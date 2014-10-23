2f40402fe9b49513e929f9ecafaf5c01b5038811
Rich Episcopo <Rich@Richies-MacBook-Pro.local>
2014-10-22 22:54:58 -0400

    Updated network discovery classes.

--- src/NetworkDiscovery/DiscoveryClient.java
+++ src/NetworkDiscovery/DiscoveryClient.java
@@ -4,4 +4,13 @@ package NetworkDiscovery;
  * Created by Rich on 10/22/14.
  */
 public class DiscoveryClient {
+
+    public DiscoveryClient() {
+        // do nothing
+    }
+
+    public void start() {
+        Thread discoveryThread = new Thread(DiscoveryThread.getInstance());
+        discoveryThread.start();
+    }
 }
