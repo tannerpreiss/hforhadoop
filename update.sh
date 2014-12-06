be306af3cf8227f6b480565131189d754167b308
Elmer Landaverde <elmer@vt.edu>
2014-12-06 15:31:53 -0500

    Add script to update html files

---
+++ update.sh
@@ -0,0 +1,3 @@
+cp ../../elmerland/dist/css/logger/styles.css html/css/logger/styles.css
+cp ../../elmerland/dist/js/logger/script.js   html/js/logger/script.js
+cp ../../elmerland/dist/logger.html           html/logger.html
