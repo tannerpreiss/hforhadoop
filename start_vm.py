15562f8f57b0379db9e95d41ce562f46cf51f5dd
Elmer Landaverde <elmer@vt.edu>
2014-12-08 04:30:04 -0500

    Fix start/stop scripts and add jar file

--- start_vm.py
+++ start_vm.py
@@ -1,10 +1,15 @@
-import os,time
+import os,time, subprocess
 
-take_snap_cmd = "VBoxManage snapshot \"goldenvm\" take master-state"
+take_snap = False
+snap_list_cmd = "VBoxManage snapshot \"goldenvm\" list | grep gossip"
+take_snap_cmd = "VBoxManage snapshot \"goldenvm\" take gossip"
+output = os.popen(snap_list_cmd).read()
+if (output == ""):
+    take_snap = True
+if (take_snap):
+    print("Take snap shot")
+    os.system(take_snap_cmd)
+    time.sleep(5)
+
 start_vm_cmd = "VBoxManage startvm \"goldenvm\""
-
-os.system(take_snap_cmd)
-time.sleep(5)
 os.system(start_vm_cmd)
-# time.sleep(25)
-# os.system(take_snap_cmd)
--- stop_vm.py
+++ stop_vm.py
@@ -3,11 +3,8 @@ import os, time
 
 shutdown_cmd = "VBoxManage controlvm \"goldenvm\" acpipowerbutton"
 
-restore_snap_cmd = "VBoxManage snapshot \"goldenvm\" restore master-state"
-
-# delete_cmd = "VBoxManage snapshot \"goldenvm\" delete master-state"
+restore_snap_cmd = "VBoxManage snapshot \"goldenvm\" restore gossip"
 
 os.system(shutdown_cmd)
 time.sleep(20)
 os.system(restore_snap_cmd)
-
