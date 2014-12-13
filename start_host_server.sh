822bbcb5353b9b338c6306a843ae086292ac5095
Elmer Landaverde <elmer@vt.edu>
2014-12-13 11:46:50 -0500

    Create host server starter script and remove log files

--- start_host_server.sh
+++ start_host_server.sh
@@ -1,7 +1,25 @@
 #!/bin/sh
 
-java -jar host_server.jar 1
-java -jar host_server.jar 2
-java -jar host_server.jar 3
-java -jar host_server.jar 4
-java -jar host_server.jar 5
+n=$1
+
+if [ "$n" = "" ]; then
+    n=5
+fi
+if [ "$n" > 5 ]; then
+    n=5
+fi
+
+if [ -e "log-host-server.txt" ]
+then
+    rm log-host-server.txt
+fi
+touch log-host-server.txt
+
+for i in `seq 1 $n`;
+do
+    cmd="java -jar host_server.jar $n & >> log-host-server.txt 2>&1"
+    echo $cmd
+    eval $cmd
+done
+
+tail -f log-host-server.txt
