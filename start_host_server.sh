9d34232136c24a1eea4cc5cbe80585b2b59230a0
Elmer Landaverde <elmer@vt.edu>
2014-12-13 12:00:32 -0500

    Update startup host script

--- start_host_server.sh
+++ start_host_server.sh
@@ -1,24 +1,29 @@
 #!/bin/sh
-
 n=$1
 
 if [ "$n" = "" ]; then
+    echo "here"
     n=5
 fi
-if [ "$n" > 5 ]; then
+if [ "$n" -gt 5 ]; then
     n=5
+fi
+if [ "$n" -lt 1 ]; then
+    n=1
 fi
 
 if [ -e "log-host-server.txt" ]
 then
     rm log-host-server.txt
 fi
+
 touch log-host-server.txt
 
+echo "Run $n host servers..."
 for i in `seq 1 $n`;
 do
-    cmd="java -jar host_server.jar $n & >> log-host-server.txt 2>&1"
-    echo $cmd
+    cmd="java -jar host_server.jar $i & >> log-host-server.txt 2>&1"
+    printf "==> Run frosty-$i: $cmd\n"
     eval $cmd
 done
 
